d40e5e0133a85aa7029827e3a4896290bce2a62a
EC2 Default User <ec2-user@ip-172-31-15-26.ap-northeast-1.compute.internal>
2022-10-21 13:30:18 +0000

    [Update]3-12

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
-  get 'books/new'
-  get 'books/index'
-  get 'books/show'
-  get 'books/edit'
+
+  get 'books/new' => 'books#new', as: 'new_book'
   get 'top' => 'homes#top'
-
   post 'books' => 'books#create'
-
+  get 'books' =>  'books#index'
+  get 'books/:id' => 'books#show', as: 'book'
+  get 'books/:id/edit' => 'books#edit', as: 'edit_book'
+  patch  'books/:id' => 'books#update', as: 'update_book'
+  delete 'books/:id' => 'books#destroy', as: 'destroy_book'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
--- db/migrate/20221015104737_create_books.rb
+++ db/migrate/20221015104737_create_books.rb
@@ -2,8 +2,8 @@ class CreateBooks < ActiveRecord::Migration[6.1]
   def change
     create_table :books do |t|
 
-      t.string :title#"本のタイトル"
-      t.string :body#"感想"
+      t.string :title
+      t.string :body
 
       t.timestamps
     end
